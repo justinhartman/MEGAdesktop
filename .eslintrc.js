7886d96c346e1c0c8fa5d9d7bb51a56c7dfe788a
Justin Hartman <code@justinhartman.co>
2021-10-08 22:55:35 +0200

    style: Remove StyleCI and Add ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true,
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12,
+    },
+    'rules': {
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'never'],
+        'comma-dangle': ['error', 'always'],
+        'comma-style': ['error', 'last'],
+        'prefer-arrow-callback': ['error'],
+    },
+};
